6a2031eec4f3699ecb00aa3ea7fc80497eaf795b
Adam Harvey <adam@adamharvey.name>
2018-11-09 11:14:59 -0800

    Update to latest Babel and stuff.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,75 +6,61 @@ export default {
   entry: "./index.js",
   output: {
     path: resolve(__dirname, "dist"),
-    publicPath: "/pages/aharvey/php-crystal-ball/",
-    filename: "pres.js",
+    publicPath: "/PRES-NAME/",
+    filename: "pres.js"
   },
-  devtool: "inline-source-map",
-  devServer: {
-    inline: true,
-    outputPath: resolve(__dirname, "dist"),
-  },
+  mode: "development",
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.css$/,
-        loader: ["style-loader", "css-loader"],
+        loader: ["style-loader", "css-loader"]
       },
       {
         test: /\.less$/,
-        loader: ["style-loader", "css-loader", "less-loader"],
+        loader: ["style-loader", "css-loader", "less-loader"]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["es2015"],
-        },
+        loader: "babel-loader"
       },
       {
-        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 50000,
-        },
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        test: /\.(png|jpg|jpeg|gif|svg|ttf)$/,
         loader: "url-loader",
         options: {
-          limit: 100000,
-        },
+          limit: 100000
+        }
       },
       {
         test: /\.html$/,
-        loader: "html-loader",
-      },
-    ],
+        loader: "html-loader"
+      }
+    ]
   },
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin,
-    new webpack.NoEmitOnErrorsPlugin,
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     // Use copy-webpack-plugin to place unbundled assets in the right place.
     new CopyWebpackPlugin([
       // The stub index.html.
       {
         from: resolve(__dirname, "index.html"),
-        to: resolve(__dirname, "dist", "index.html"),
+        to: resolve(__dirname, "dist", "index.html")
       },
       // Various other dependencies.
       {
         from: resolve(__dirname, "images"),
-        to: resolve(__dirname, "dist", "images"),
+        to: resolve(__dirname, "dist", "images")
       },
       {
         from: resolve(__dirname, "node_modules", "reveal.js", "lib"),
-        to: resolve(__dirname, "dist", "node_modules", "reveal.js", "lib"),
+        to: resolve(__dirname, "dist", "node_modules", "reveal.js", "lib")
       },
       {
         from: resolve(__dirname, "node_modules", "reveal.js", "plugin"),
-        to: resolve(__dirname, "dist", "node_modules", "reveal.js", "plugin"),
-      },
-    ]),
-  ],
+        to: resolve(__dirname, "dist", "node_modules", "reveal.js", "plugin")
+      }
+    ])
+  ]
 };
